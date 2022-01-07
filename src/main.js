04be2fea490a90e04fc786cd517364ef551e415f
fangjiacan <845621557@qq.com>
2022-01-08 05:50:16 +0800

    feat：vuex配置

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router/index'
+import store from './store/index'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
+app.use(store)
 app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+
+import { createStore } from 'vuex'
+import user from './model/user'
+
+export default createStore({
+  modules: {
+    user
+  }
+})
